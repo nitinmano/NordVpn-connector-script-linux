af5bab7443c64952969e0472b3d35e98c61919e0
nitinmano <55648552+nitinmano@users.noreply.github.com>
2021-06-15 02:06:21 +0530

    optimization

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from os import system as s
-print("Enter the options\n1.Nearest Server\n2.US\n3.UK\n4.Taiwan\n5.Select Others\n6.Disconnect")
-print("7.OtherCommands")
+print("Enter the options\n1.Nearest Server\n2.US\n3.UK\n4.Taiwan\n5.Select Other Countries\n6.Disconnect")
+print("7.OtherCommands\n8.Exit")
+src="nordvpn "
 while(True):
     try:
         n=int(input())
-        src="nordvpn"
         if(n==1):
-            s(src,"c")
+            s(src+"c")
         elif(n==2):
-            s(src,"c us")
+            s(src+"c us")
         elif(n==3):
-            s(src,"c uk")
+            s(src+"c uk")
         elif(n==4):
-            s(src,"c taiwan")
+            s(src+"c taiwan")
         elif(n==5):
-            s(src)
-            s(src,input())
+            s(src+"countries")
+            print("Type the Country name:")
+            s(src+"c "+input())
         elif(n==6):
-            s(src,"d")
+            s(src+"d")
         elif(n==7):
-            s(src,"h")
-            s(src,input())
-        exit(0)
+            s(src+"h")
+            print("nordvpn",end=" ")
+            s(src+input())
+        else:
+            break
+        break
     except:
-        pass
-
+        print("Try Again")
